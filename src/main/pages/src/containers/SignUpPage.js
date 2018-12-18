1bc294ae9f87eecf597a04a4ff11ea81a1786e60
duketran1996 <duketran1996@gmail.com>
2018-12-18 00:32:44 -0500

    feat: rework sign up page with material ui

--- src/main/pages/src/containers/SignUpPage.js
+++ src/main/pages/src/containers/SignUpPage.js
@@ -1,75 +1,233 @@
 import React from 'react';
-import '../styles/AcountPageCustom.css';
-import {NavLink} from "react-router-dom";
-import Email from '@material-ui/icons/Email';
-import Lock from '@material-ui/icons/Lock';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import orange from '@material-ui/core/colors/orange';
+import grey from '@material-ui/core/colors/grey';
 
-class SignUpPage extends React.Component {
+const styles = theme => ({
+    root: {
+        color: orange[600],
+        '&$checked': {
+            color: orange[500],
+        },
+    },
+    checked: {},
+    title: {
+        fontSize: '80px',
+        marginTop: theme.spacing.unit * 15,
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 350,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 2,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+        borderRadius: '20px',
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: '#212121',
+        width: 60,
+        height: 60,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+        color: 'black',
+    },
+    formControl:{
+        '&:focus': {
+            fontWeight: 'bold',
+            backgroundColor: orange[500],
+        },
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 2.5,
+        color: theme.palette.getContrastText(orange[300]),
+        backgroundColor: orange[300],
+        '&:hover': {
+            fontWeight: 'bold',
+            fontSize: '15px',
+            backgroundColor: orange[500],
+        },
+        '&:focus': {
+            outline: 'none',
+        }
+    },
+    signinmessage: {
+        marginTop: theme.spacing.unit * 3,
+    },
+    signinlink: {
+        color: grey[900],
+        '&:hover': {
+            fontWeight: 'bold',
+            fontSize: '16px',
+            color: orange[500],
+        },
+
+    },
+    cssLabel: {
+        '&$cssFocused': {
+            fontWeight: 'bold',
+            color: orange[500],
+        },
+    },
+    cssFocused: {},
+    cssUnderline: {
+        '&:after': {
+            fontWeight: 'bold',
+            borderBottomColor: orange[500],
+        },
+    },
+});
+
+class LoginPage extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
             username: '',
             password: '',
-            usertype:''
+            type: '',
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(){
-        this.props.onSubmit(this.state.username,this.state.password,this.state.usertype);
+        this.props.onSubmit(this.state.username,this.state.password);
     }
 
+    handleChange = event => {
+        this.setState({ type: event.target.value });
+    };
+
     render(){
+        const { classes } = this.props;
+
         return (
-            <div className="container">
-                <div className="d-flex justify-content-center title">
-                    <h1>Whiteboard</h1>
-                </div>
-                <div className="card">
-                    <div className="card-header">
-                        <h3>Sign Up</h3>
-                    </div>
-                    <div className="card-body">
-                        <form>
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><Email className="email_icon"></Email></span>
-                                </div>
-                                <input type="text" className="form-control" placeholder="Email" onChange={(event) => this.setState({username:event.target.value})}/>
-
-                            </div>
-
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><Lock className="lock_icon"></Lock></span>
-                                </div>
-                                <input type="text" className="form-control" placeholder="Password" onChange={(event) => this.setState({password:event.target.value})}/>
-                            </div>
-
-                            <div className="row align-items-center type">
-                                <div className="radio">
-                                    <label><input type="radio" name="optradio" onChange={(event) => this.setState({usertype:'Professor'})}/>&nbsp;Professor</label>
-                                </div>
-                                <div className="radio">
-                                    <label><input type="radio" name="optradio" onChange={(event) => this.setState({usertype:'Student'})}/>&nbsp;Student</label>
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <input type="submit" value="Register" onClick={this.handleChange} className="btn float-right register_btn"/>
-                            </div>
+            <div>
+                <Typography className={classes.title}>
+                    Whiteboard
+                </Typography>
+                <main className={classes.main}>
+                    <CssBaseline />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <PersonAdd style={{ fontSize: 50 }} />
+                        </Avatar>
+                        <form className={classes.form}>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel
+                                    htmlFor="email"
+                                    classes={{
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    }}
+                                >Email Address</InputLabel>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    classes={{
+                                        underline: classes.cssUnderline,
+                                    }}
+                                />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel
+                                    htmlFor="password"
+                                    classes={{
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    }}
+                                >Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    classes={{
+                                        underline: classes.cssUnderline,
+                                    }}
+                                />
+                            </FormControl>
+                            <FormControlLabel
+                                control={<Radio
+                                    checked={this.state.type === 'professor'}
+                                    onChange={this.handleChange}
+                                    value="professor"
+                                    classes={{
+                                        root: classes.root,
+                                        checked: classes.checked,
+                                    }}
+                                />}
+                                label="Professor"
+                            />
+                            <FormControlLabel
+                                control={<Radio
+                                    checked={this.state.type === 'student'}
+                                    onChange={this.handleChange}
+                                    value="student"
+                                    classes={{
+                                        root: classes.root,
+                                        checked: classes.checked,
+                                    }}
+                                />}
+                                label="Student"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                className={classes.submit}
+                            >
+                                Register
+                            </Button>
+                            <Typography
+                                className={classes.signinmessage}
+                                align="center"
+                            >
+                                Already have and account?&nbsp;<NavLink exact to = "/" className={classes.signinlink}>Sign In</NavLink>
+                            </Typography>
                         </form>
-                    </div>
-                    <div className="card-footer">
-                        <div className="d-flex justify-content-center links account-text">
-                            Already have an account?&nbsp;<NavLink exact to = "/" className='login-link'>Login In</NavLink>
-                        </div>
-                    </div>
-                </div>
-
+                    </Paper>
+                </main>
             </div>
         );
     }
-
 }
-export default SignUpPage;
+
+LoginPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LoginPage);
